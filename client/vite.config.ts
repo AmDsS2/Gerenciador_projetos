320ebd0474e3389d3079d4b358b42032fe975be5
Aryan Martins <aryan@r10consultoria.com.br>
2025-05-27 10:56:57 -0300

    Configurando drizzle-orm como dependência externa no Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
+      external: ["drizzle-orm", "drizzle-orm/pg-core"],
       output: {
         manualChunks: {
           vendor: ["react", "react-dom", "react-router-dom"],
